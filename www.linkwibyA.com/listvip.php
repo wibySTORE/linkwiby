511c666aeee3cf28e76e3ba94740f366fe52fff1
wiby <muhamadtama619@gmail.com>
2024-08-13 19:56:01 +0700

    Add files via upload

---
+++ www.linkwibyA.com/listvip.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>
+            list vip
+        </title>
+        <link rel="shortcut icon" type="x-icon" href="profil.png">
+    </head>
+  <body>
+
+<style>
+
+        .tengah{
+            width: 100%;
+            float:right;
+        }
+
+</style>
+
+
+<!-- di bawah ini adalah style title -->
+    <b><h1 align="center">HARGA CONFIG VIP ADA DISINI MAS BRO KU</h1></b>
+
+<!-- di bawha ini adalah gambar list -->
+    <span class=""><img src="image.jpg.jpeg" alt=""></span>
+
+<h1><li>PAYMENT</li></h1>
+<p><h2>
+<li>dana</li>
+<li>ovo</li>
+<li>gopay</li>
+<li>qris</li>
+</p></h2>
+                                        
+</body>
+</html>
